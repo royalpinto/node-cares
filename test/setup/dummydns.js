7a903807b535f1b2639f4eacd82bfdaf720ef900
Royal Pinto <royalpinto@gmail.com>
2016-05-13 20:20:48 +0530

    ESLint corrections for dummydns.js.

--- test/setup/dummydns.js
+++ test/setup/dummydns.js
@@ -3,26 +3,25 @@ var dnsentries = require('./dnsentries.js');
 
 var server = dns.createUDPServer();
 
-server.on('request', function (request, response) {
-
+server.on('request', function(request, response) {
     var question = request.question[0];
 
-    var class_data = dnsentries[question.class];
-    var rrtype_data = class_data[question.type];
+    var classData = dnsentries[question.class];
+    var rrtypeData = classData[question.type];
 
-    (function () {
-        var answer = rrtype_data[question.name]['answer'];
+    (function() {
+        var answer = rrtypeData[question.name].answer;
         if (answer) {
-            answer.forEach(function (entry) {
-            response.answer.push(entry);
+            answer.forEach(function(entry) {
+                response.answer.push(entry);
             });
         }
     })();
 
-    (function () {
-        var additional = rrtype_data[question.name]['additional'];
+    (function() {
+        var additional = rrtypeData[question.name].additional;
         if (additional) {
-            additional.forEach(function (entry) {
+            additional.forEach(function(entry) {
                 response.additional.push(entry);
             });
         }
@@ -31,7 +30,7 @@ server.on('request', function (request, response) {
     response.send();
 });
 
-server.on('error', function (err, buff, req, res) {
+server.on('error', function(err) {
     console.error(err.stack);
 });
 
